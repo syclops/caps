f2ab4117c2b5fb8253d3e29e6c4e39bf93bfdc33
Yucheng Dai <yucheng_dai@126.com>
2019-08-15 18:21:37 +0800

    New tests.

--- src/signaling/encoding/fsa_encoder/fsa_mixed_huffman_encoder.h
+++ src/signaling/encoding/fsa_encoder/fsa_mixed_huffman_encoder.h
@@ -42,11 +42,10 @@ class FSAMixedHuffmanEncoder: public FSAEncoder<BitVectorType>
     auto temp_string_coder = std::make_shared<HuffmanCoder<std::string,
                                                      BitVectorType>>(get_label_counts(lexicon));
     LabeledGraph::LabelMap counts3{};
-    size_t cnt=0;
+    size_t cnt=0, saved=0;
     for (auto& [symbol, count]: counts1){
       if (temp_char_coder->value_size(symbol)>temp_string_coder->value_size(symbol)){
         counts3.emplace(symbol, count);
-        cnt++;
       }else{
         for(auto c: symbol){
           if (counts2.find(c)!=counts2.end()) counts2.at(c) += count;
@@ -54,9 +53,12 @@ class FSAMixedHuffmanEncoder: public FSAEncoder<BitVectorType>
         }
         if (counts2.find(END_OF_STRING)!=counts2.end()) counts2.at(END_OF_STRING)+=count;
         else counts2.emplace(END_OF_STRING, count);
+        cnt++;
+        saved+=(temp_string_coder->value_size(symbol)-temp_char_coder->value_size(symbol))*count;
       }
     }
     std::cout<<"cnt: "<<cnt<<std::endl;
+    std::cout<<"saved: "<<saved<<std::endl;
     auto label_coder = std::make_shared<MixedHuffmanCoder<std::string,
                                                      BitVectorType>>(counts3, counts2);
     using LabelCoderType = Coder<std::string, BitVectorType>;
