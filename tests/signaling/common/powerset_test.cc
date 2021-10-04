d62bd6c14569b945ab4cb41ea331117b9bd759d1
Micah <micahhelenreid@gmail.com>
2021-10-04 14:29:41 -0400

    powerset test of contains written (specific, not general_

--- tests/signaling/common/powerset_test.cc
+++ tests/signaling/common/powerset_test.cc
@@ -9,13 +9,33 @@
 
 #include "gtest/gtest.h"
 
-TEST(PowersetTest, SetTest)
+TEST(PowersetTest, SetSize)
 {
   std::set<int> set = {1, 2, 3};
   auto pset = powerset(set);
   ASSERT_EQ(8, pset.size());
-  // TODO: check that every expected subset is in the set.
+    std::set<int> set2 = {1, 2, 3, 4};
+    auto pset2 = powerset(set2);
+    ASSERT_EQ(16, pset2.size());
 }
+TEST(PowersetTest, SetContains)
+{
+    std::set<int> set = {1, 2, 3};
+    auto pset = powerset(set);
+    std::set<int> set1 = {};
+    std::set<int> set2 = {1};
+    std::set<int> set3 = {2};
+    std::set<int> set4 = {3};
+    std::set<int> set5 = {1, 2};
+    std::set<int> set6 = {1, 3};
+    std::set<int> set7 = {2, 3};
+    std::set<int> set8 = {1, 2, 3};
+    std::set<std::set<int>> pset2 = {set1, set2, set3, set4, set5, set6, set7, set8};
+
+
+ASSERT_EQ(pset2, pset);
+}
+
 
 // TODO: generalize this test to take a set of generic type as a parameter
 //       find a way to generalize checking that each subset is in the set.
